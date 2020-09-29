5cb5c6f41e0ab209ac458d51b613d89426151ef6
Trece 13 <juanccubillos@gmail.com>
2020-09-28 20:51:15 -0500

    Actualizaciones api

--- WebApplication8/Controllers/HistoryController.cs
+++ WebApplication8/Controllers/HistoryController.cs
@@ -12,6 +12,10 @@ using Newtonsoft.Json;
 using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Cors;
 using FireSharp.Extensions;
+using System.Globalization;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 
 namespace WebApplication8.Controllers
 {
@@ -20,7 +24,8 @@ namespace WebApplication8.Controllers
     public class HistoryController : ControllerBase
     {
         private readonly ILogger<HistoryController> _logger;
-
+        Dictionary<string, History> Historydictionary;
+        List<History> Histories = new List<History>();
         public HistoryController(ILogger<HistoryController> logger)
         {
             _logger = logger;
@@ -33,16 +38,34 @@ namespace WebApplication8.Controllers
         };
 
         [HttpGet]
-        public string Get()
+        public string Get(string User)
         {
             List<History> histories = new List<History>();
             var client = new FireSharp.FirebaseClient(config);
             FirebaseResponse responce = client.Get("firetest");
-            return JsonConvert.SerializeObject(responce.Body);
+            Historydictionary = responce.ResultAs<Dictionary<string, History>>();
+            
+            if (Historydictionary != null)
+            {
+                foreach(var item in Historydictionary)
+                {
+                    if (item.Value.User == User)
+                    {
+                        History history = new History();
+                        history.Id = item.Key;
+                        history.Date = item.Value.Date;
+                        history.User = item.Value.User;
+                        history.Screen = item.Value.Screen;
+
+                        Histories.Insert(0,history);
+                    }
+                }
+            }
+            return JsonConvert.SerializeObject(Histories);
         }
 
         [HttpPost]
-        public void Addistoria(History history)
+        public void Post([FromBody]History history)
         {
             var client = new FireSharp.FirebaseClient(config);
             PushResponse responce = client.Push("firetest/", history);
--- WebApplication8/Models/History.cs
+++ WebApplication8/Models/History.cs
@@ -4,8 +4,13 @@ namespace WebApplication8
 {
     public class History
     {
+        public History()
+        {
+            this.Date = DateTime.Now.ToString("MM/dd/yyyy H:mm:ss");
+        }
+        public string? Id { get; set; }
         public string User { get; set; }
-        public DateTime Date { get; set; }
+        public string? Date { get; set; }
         public string Screen { get; set; }
 
 
